04c904bc30431e2bae00ae50ecae0ee2b5ca7460
AppAnge1008 <812474076@qq.com>
2017-10-07 14:50:50 +0800

    新建人才培养方案组件

--- src/app/business-module/business-module.module.ts
+++ src/app/business-module/business-module.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MajorManageComponent } from './major-manage/major-manage.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TrainingComponent } from './training/training.component';
 
 //定义系统路由
 const routes: Routes = [
   {
     path : 'majormanage',
     component: MajorManageComponent
+  },{
+    path : 'majortrain',
+    component: TrainingComponent
   }
 ];
 
@@ -17,7 +21,7 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forRoot(routes),
   ],
-  declarations: [MajorManageComponent],
-  exports:[MajorManageComponent]
+  declarations: [MajorManageComponent, TrainingComponent],
+  exports:[MajorManageComponent,TrainingComponent]
 })
 export class BusinessModuleModule { }
